3f5abf31b4230c100a83696aa60f41365bcdaf07
caojing <1178440753@qq.com>
2017-12-01 23:16:20 +0800

    分类模块完成

--- public/admin/js/second.js
+++ public/admin/js/second.js
@@ -11,7 +11,7 @@ $(function(){
 	function render(){
 		$.ajax({
 		type:"get",
-		url:"/category/queryTopCategoryPaging",
+		url:"/category/querySecondCategoryPaging",
 		data:{
 			page:page,
 			pageSize:pageSize
@@ -31,7 +31,6 @@ $(function(){
 				bootstrapMajorVersion: 3 ,//搭配使用的bootstrap版本，3的话就得用ul
 				currentPage: page,
 				totalPages:Math.ceil(data.total/data.size),
-				numberOfPages:5,
 				onPageClicked:function(a,b,c,p){
 					// 修改当前页码
 					page=p;
@@ -51,7 +50,48 @@ render();
 
 // 添加分类功能，显示模态框
 ;$(".btn_add").on("click",function(){
-	$("#firstModal").modal("show");
+	$("#secondModal").modal("show");
+
+	// 需要发送ajax请求，获取到所有的一级分类，渲染到下拉菜单中
+	$.ajax({
+		type:"get",
+		url:"/category/queryTopCategoryPaging",
+		data:{
+			page:1,
+			pageSize:100
+		},
+		success:function(data){
+			console.log(data);
+			$(".dropdown-menu").html(template("tpl2",data));
+		}
+	})
+});
+
+
+
+// 需要给下拉框中所有的a标签注册点击事件
+$(".dropdown-menu").on("click","a",function(){
+	// 获取到当前a标签的文本，设置给按钮的文本
+	$(".dropdown-text").text($(this).text());
+
+	// 获取到id值，设置给categoryId
+	$("[name='categoryId']").val($(this).data("id"));
+
+	// 让categoryId校验成功
+	$form.data("bootstrapValidator").updateStatus("categoryId","VALID");
+});
+
+
+// 图片上传
+$("#fileupload").fileupload({
+	dataType:"json",
+	done:function(e,data){
+		$(".img_box img").attr("src",data.result.picAddr);
+
+		$("[name='brandLogo']").val(data.result.picAddr);
+
+		$form.data("bootstrapValidator").updateStatus("brandLogo","VALID");
+	}
 });
 
 
@@ -63,6 +103,7 @@ render();
 var $form=$('form');
 
 $form.bootstrapValidator({
+		excluded:[],
 
 		// 小图标
 		feedbackIcons: {
@@ -74,10 +115,28 @@ $form.bootstrapValidator({
 
 		// 校验字段
 		fields:{
-		categoryName:{
+		categoryId:{
 			validators:{
 				notEmpty:{
-					message:'一级分类名称不能为空'
+					message:'请选择一级分类'
+				}
+				
+			}
+		},
+
+		brandName:{
+			validators:{
+				notEmpty:{
+					message:'请输入二级分类名称'
+				}
+				
+			}
+		},
+
+		brandLogo:{
+			validators:{
+				notEmpty:{
+					message:'请上传图片'
 				}
 				
 			}
@@ -100,14 +159,14 @@ $form.bootstrapValidator({
 		// 使用ajax提交逻辑
 		$.ajax({
 			type:"post",
-			url:"/category/addTopCategory",
+			url:"/category/addSecondCategory",
 			dataType:'json',//如果后端返回的响应头有text/html
 			data:$form.serialize(),
 			success:function(data){
 				if(data.success){
 					
 					// 关闭模态框
-					$("#firstModal").modal("hide");
+					$("#secondModal").modal("hide");
 
 					// 重新渲染第一页，因为新增的分类在第一页
 					page=1;
@@ -117,6 +176,11 @@ $form.bootstrapValidator({
 					$form.data("bootstrapValidator").resetForm();
 					// 重置表单的value值
 					$form[0].reset();
+
+
+					$(".dropdown-text").text("请选择一级分类");
+					$(".img_box img").attr("src","images/none.png");
+					$("[type='hidden']").val('');
 				}
 
 				
